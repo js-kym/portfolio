a4f3ccedd5e09bcb4082251ce3827193c1876ff3
kym <fr3020.obe@gmail.com>
2019-06-18 01:20:39 +0900

    [update]TOPページをpagesのみで表示するように修正

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="pages-top">
-    <div v-if="$store.state.width >= 1000">
-      <top/>
+    <div
+      v-if="$store.state.width >= 1000"
+      class="contents-top-pc">
+      <div class="contents-top-pc__main">
+        <vue-typer
+          :text="['kym\'s Portfolio', 'Frontend Engineer', 'Flasher']"
+          :pre-type-delay='70'
+          :type-delay='150'
+          :erase-delay='100'
+          erase-style='clear'
+        ></vue-typer>
+      </div>
     </div>
-    <div v-else>
-      <top-sp/>
+    <div
+      v-else
+      class="contents-top-sp">
+      <div class="contents-top-sp__main">
+        <div>kym's Portfolio</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Top from '../contents/Top.vue';
-import TopSp from '../contents/TopSp.vue';
+import { VueTyper } from 'vue-typer';
 
 export default {
   name: 'pages-top',
   components: {
-    Top,
-    TopSp
+    VueTyper
   }
 };
 </script>
@@ -28,8 +40,41 @@ export default {
   width: 100%;
   height: calc(100vh - 70px - 36px);
 }
-.pages-top div {
+/* PC用 */
+.contents-top-pc {
   width: 100%;
   height: 100%;
+  color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.contents-top-pc__main {
+  position: relative;
+  width: 1000px;
+  text-align: center;
+}
+.vue-typer {
+  font-family: 'Play', sans-serif;
+  font-size: 4rem;
+}
+.vue-typer .custom.char.selected {
+  background-color: #ffcd3c;
+}
+/* SP用 */
+.contents-top-sp {
+  width: 100%;
+  height: 100%;
+  color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.contents-top-sp__main {
+  position: relative;
+  width: 100%;
+  text-align: center;
+  font-family: 'Play', sans-serif;
+  font-size: 2rem;
 }
 </style>
